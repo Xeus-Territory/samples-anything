20f737f167d8655ddf3ee4664305577e0fef0a67
Xeus - Nghia Nguyen <kobikiu810@gmail.com>
2023-12-31 09:43:44 +0700

    Add a terminal profile of pwsh and zsh

---
+++ templates/terminal/powershell_profile.ps1
@@ -0,0 +1,17 @@
+## To running this script - need to bypass restricted
+## RUN THIS COMMAND VIA ADMINSTRATOR PWSH: Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Set the autosuggestion in pwsh
+# Install before running (Administrator running)        
+# Install-Module PSReadLine -RequiredVersion 2.1.0      
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource Histor
+
+# Shows navigable menu of all options when hitting Tab  
+# Default not need to downloading
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Set the autosuggestion git in pwsh
+# Install before used for preventing error
+# PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+Add-PoshGitToProfile -AllHosts -Force
